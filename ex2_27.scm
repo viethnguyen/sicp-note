3e68f66717493acdc7379ed056e6b5a424908c1b
Viet H Nguyen <viet.nguyen182@gmail.com>
2014-05-10 08:07:48 -0400

    new exercises

--- ex2_27.scm
+++ ex2_27.scm
@@ -7,7 +7,40 @@
   (list length structure))
 
 (define (left-branch m) (car m))
-(define (right-branch m) (cdr m))
+(define (right-branch m) (car (cdr m)))
 (define (branch-length b) (car b))
-(define (branch-structure b) (cdr b))
+(define (branch-structure b) (car (cdr b)))
+(define (is-mobile? s) (pair? s))
 
+
+(define (branch-weight branch)
+  (let ((s (branch-structure branch)))
+    (if (is-mobile? s)
+        (total-weight s)
+        s)))
+
+(define (total-weight m)
+  (+ (branch-weight (left-branch m))
+     (branch-weight (right-branch m))))
+
+ ;; A test mobile: 
+ ;; Level 
+ ;; ----- 
+ ;; 3                   4  |    8                                      
+ ;;              +---------+--------+ 2                        
+ ;; 2         3  |  9                                        
+ ;;        +-----+----+ 1                                    
+ ;; 1    1 | 2                                       
+ ;;    +---+---+                             
+ ;;    2       1                             
+  
+ (define level-1-mobile (make-mobile (make-branch 1 2) 
+                                     (make-branch 2 1))) 
+ (define level-2-mobile (make-mobile (make-branch 3 level-1-mobile) 
+                                     (make-branch 9 1))) 
+ (define level-3-mobile (make-mobile (make-branch 4 level-2-mobile) 
+                                     (make-branch 8 2))) 
+  
+ (total-weight level-1-mobile) 
+ (total-weight level-2-mobile) 
+ (total-weight level-3-mobile) 
